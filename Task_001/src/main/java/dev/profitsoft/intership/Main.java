2c54ab487efdb131fba109e10b207e0c68525a16
eXetrum <shishimorovmaksim@gmail.com>
2024-04-04 21:30:27 +0300

    update usage info

--- Task_001/src/main/java/dev/profitsoft/intership/Main.java
+++ Task_001/src/main/java/dev/profitsoft/intership/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         if(args.length != 2) {
             System.out.println("Usage: java -jar BooksAnalyzer.jar <json folder path> <attribute name>");
+            System.out.println("\t<attribute name>: title, subject, author, publish_year");
             return;
         }
 
