8461c8aae6e26acacc225486d5dc6c260a4f3be7
eXetrum <shishimorovmaksim@gmail.com>
2024-04-12 19:41:41 +0300

    reflection for cmd args (attr name)

--- Task_001/src/main/java/dev/profitsoft/intership/Main.java
+++ Task_001/src/main/java/dev/profitsoft/intership/Main.java
@@ -1,10 +1,21 @@
 package dev.profitsoft.intership;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
         if(args.length != 2) {
             System.out.println("Usage: java -jar BooksAnalyzer.jar <json folder path> <attribute name>");
-            System.out.println("\t<attribute name>: title, subject, author, publish_year");
+            Class<?> bookClass = Book.class;
+            System.out.println("\t<attribute name>: " +
+                    Arrays.stream(bookClass.getDeclaredFields())
+                            .filter(e -> !Modifier.isStatic(e.getModifiers()))
+                            .map(Field::getName)
+                            .collect(Collectors.joining(", "))
+            );
             return;
         }
 
