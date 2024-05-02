c49ee9410d059982fcc055e3313ab477578f0f79
eXetrum <shishimorovmaksim@gmail.com>
2024-05-02 23:54:51 +0300

    Update BookControllerTest.java

--- Task2/src/test/java/dev/profitsoft/intership/BookControllerTest.java
+++ Task2/src/test/java/dev/profitsoft/intership/BookControllerTest.java
@@ -1,5 +1,6 @@
 package dev.profitsoft.intership;
 
+import com.jayway.jsonpath.JsonPath;
 import dev.profitsoft.intership.booklibrary.BookLibraryApplication;
 import dev.profitsoft.intership.booklibrary.data.AuthorData;
 import dev.profitsoft.intership.booklibrary.repository.AuthorRepository;
@@ -12,6 +13,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
 import java.time.Instant;
 import java.util.ArrayList;
@@ -56,6 +58,7 @@ public class BookControllerTest {
     @AfterEach
     public void afterEach() {
         bookRepository.deleteAll();
+        authorRepository.deleteAll();
     }
 
     @Test
@@ -75,4 +78,163 @@ public class BookControllerTest {
                 .andExpect(status().isCreated());
 
     }
+
+    @Test
+    public void testCreateBookDuplicate() throws Exception {
+        String body = """
+               {
+                    "title": "test book",
+                    "genre": "fiction",
+                    "publishYear": 1999,
+                    "authorId": "%s"
+               }
+               """.formatted(defaultAuthors.get(0).getId());
+        mvc.perform(post("/api/book")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(body)
+                )
+                .andExpect(status().isCreated());
+
+        mvc.perform(post("/api/book")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(body)
+                )
+                .andExpect(status().isBadRequest());
+
+    }
+
+    @Test
+    public void testCreateBookAuthorNotFound() throws Exception {
+        String body = """
+               {
+                    "title": "test book",
+                    "genre": "fiction",
+                    "publishYear": 1999,
+                    "authorId": "random_author_id_which_is_not_found"
+               }
+               """;
+        mvc.perform(post("/api/book")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(body)
+                )
+                .andExpect(status().isNotFound());
+
+    }
+
+    @Test
+    public void testCreateBookBadFormat() throws Exception {
+        String body = """
+               {
+                    "title-title": "test book",
+                    "genre-baz": "fiction",
+                    "publishYear-buz": 1999
+               }
+               """;
+        mvc.perform(post("/api/book")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(body)
+                )
+                .andExpect(status().isBadRequest());
+
+    }
+
+    @Test
+    public void testGetBookById() throws Exception {
+        String body = """
+               {
+                    "title": "test book",
+                    "genre": "fiction",
+                    "publishYear": 1999,
+                    "authorId": "%s"
+               }
+               """.formatted(defaultAuthors.get(0).getId());
+       MvcResult result = mvc.perform(post("/api/book")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(body)
+                )
+                .andExpect(status().isCreated()).andReturn();
+
+        String jsonResponse = result.getResponse().getContentAsString();
+        String id = JsonPath.read(jsonResponse, "$.result");
+
+        mvc.perform(get("/api/book/" + id)
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.title").value("test book"))
+                .andExpect(jsonPath("$.genre").value("fiction"))
+                .andExpect(jsonPath("$.publishYear").value("1999"))
+                .andExpect(jsonPath("$.author.name").value(defaultAuthors.get(0).getName()))
+                .andExpect(jsonPath("$.author.birthdayYear").value(defaultAuthors.get(0).getBirthdayYear()))
+                .andExpect(jsonPath("$.author.id").value(defaultAuthors.get(0).getId()));
+
+    }
+
+    @Test
+    public void testGetBookByIdNotFound() throws Exception {
+        mvc.perform(get("/api/book/random_id")
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isNotFound());
+
+    }
+
+    @Test
+    public void testUpdateBookById() throws Exception {
+        String body = """
+               {
+                    "title": "test book",
+                    "genre": "fiction",
+                    "publishYear": 1999,
+                    "authorId": "%s"
+               }
+               """.formatted(defaultAuthors.get(0).getId());
+        MvcResult result = mvc.perform(post("/api/book")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(body)
+                )
+                .andExpect(status().isCreated()).andReturn();
+
+        String jsonResponse = result.getResponse().getContentAsString();
+        String id = JsonPath.read(jsonResponse, "$.result");
+
+        mvc.perform(get("/api/book/" + id)
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.title").value("test book"))
+                .andExpect(jsonPath("$.genre").value("fiction"))
+                .andExpect(jsonPath("$.publishYear").value("1999"))
+                .andExpect(jsonPath("$.author.name").value(defaultAuthors.get(0).getName()))
+                .andExpect(jsonPath("$.author.birthdayYear").value(defaultAuthors.get(0).getBirthdayYear()))
+                .andExpect(jsonPath("$.author.id").value(defaultAuthors.get(0).getId()));
+
+
+        mvc.perform(put("/api/book/" + id)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                        {
+                        "title": "NEW test book",
+                        "genre": "horror",
+                        "publishYear": 1111,
+                        "authorId": "%s"
+                        }
+                        """.formatted(defaultAuthors.get(1).getId()))
+                )
+                .andExpect(status().isOk());
+
+
+        mvc.perform(get("/api/book/" + id)
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.title").value("NEW test book"))
+                .andExpect(jsonPath("$.genre").value("horror"))
+                .andExpect(jsonPath("$.publishYear").value("1111"))
+                .andExpect(jsonPath("$.author.name").value(defaultAuthors.get(1).getName()))
+                .andExpect(jsonPath("$.author.birthdayYear").value(defaultAuthors.get(1).getBirthdayYear()))
+                .andExpect(jsonPath("$.author.id").value(defaultAuthors.get(1).getId()));
+
+    }
+
 }
