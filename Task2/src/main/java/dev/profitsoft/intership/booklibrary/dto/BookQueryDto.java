27a5c72f108a5372a24f71bfd36ccce2efe717bd
eXetrum <shishimorovmaksim@gmail.com>
2024-05-13 16:54:34 +0300

    Update BookQueryDto.java

--- Task2/src/main/java/dev/profitsoft/intership/booklibrary/dto/BookQueryDto.java
+++ Task2/src/main/java/dev/profitsoft/intership/booklibrary/dto/BookQueryDto.java
@@ -8,9 +8,12 @@ import lombok.extern.jackson.Jacksonized;
 @Setter
 @Jacksonized
 public class BookQueryDto {
+    private String title;
+    private String genre;
+    private Integer publishYear;
     private String authorId;
     private String authorName;
-    private int page = 0;
-    private int size = 5;
+    private Integer page = 0;
+    private Integer size = 5;
 }
 
