8fb455de6458f4ca45debadec76488d862252b56
eXetrum <shishimorovmaksim@gmail.com>
2024-05-13 16:56:14 +0300

    Update BookRepository.java

--- Task2/src/main/java/dev/profitsoft/intership/booklibrary/repository/BookRepository.java
+++ Task2/src/main/java/dev/profitsoft/intership/booklibrary/repository/BookRepository.java
@@ -1,32 +1,23 @@
 package dev.profitsoft.intership.booklibrary.repository;
 
 import dev.profitsoft.intership.booklibrary.data.BookData;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
-public interface BookRepository extends JpaRepository<BookData, String> {
+public interface BookRepository extends JpaRepository<BookData, String>, JpaSpecificationExecutor<BookData> {
 
-    @Query("SELECT book FROM BookData AS book WHERE book.author.id = :authorId ORDER BY book.title")
-    Page<BookData> findByAuthorId(@Param("authorId") String authorId, Pageable pageable);
-
-    @Query("SELECT book FROM BookData AS book WHERE LOWER(book.author.name) LIKE LOWER(concat('%', :authorName, '%')) ORDER BY book.title")
-    Page<BookData> findByAuthorName(@Param("authorName") String authorName, Pageable pageable);
-
-    @Query("SELECT book FROM BookData AS book " +
+    @Query("SELECT COUNT(book) = 1 FROM BookData AS book " +
             "WHERE book.title = :title " +
             "AND book.genre = :genre " +
             "AND book.publishYear = :publishYear " +
             "AND book.author.name = :authorName " +
             "AND book.author.birthdayYear = :authorBirthdayYear"
     )
-    List<BookData> findByFullDescription(
+    boolean existsBookDataByFullDescription(
             @Param("title") String title,
             @Param("genre") String genre,
             @Param("publishYear") Integer publishYear,
