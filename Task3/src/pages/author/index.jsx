533887aef32aaaf7e91d9222aa3b985a14b1c748
eXetrum <shishimorovmaksim@gmail.com>
2024-05-14 20:17:00 +0300

    author page init

---
+++ Task3/src/pages/author/index.jsx
@@ -0,0 +1,20 @@
+import React, { useMemo } from 'react';
+import IntlProvider from 'misc/providers/IntlProvider';
+import useLocationSearch from 'misc/hooks/useLocationSearch';
+
+import getMessages from './intl';
+import Author from './containers/Author';
+
+function Index(props) {
+  const {
+    lang,
+  } = useLocationSearch();
+  const messages = useMemo(() => getMessages(lang), [lang]);
+  return (
+    <IntlProvider messages={messages}>
+      <Author {...props} />
+    </IntlProvider>
+  );
+}
+
+export default Index;
