38a59560272321992bb0d7a3c04866ce496d95c3
eXetrum <shishimorovmaksim@gmail.com>
2024-05-14 20:17:17 +0300

    book-list page init

---
+++ Task3/src/pages/book-list/index.jsx
@@ -0,0 +1,20 @@
+import React, { useMemo } from 'react';
+import IntlProvider from 'misc/providers/IntlProvider';
+import useLocationSearch from 'misc/hooks/useLocationSearch';
+
+import getMessages from './intl';
+import BookList from './containers/BookList';
+
+function Index(props) {
+  const {
+    lang,
+  } = useLocationSearch();
+  const messages = useMemo(() => getMessages(lang), [lang]);
+  return (
+    <IntlProvider messages={messages}>
+      <BookList {...props} />
+    </IntlProvider>
+  );
+}
+
+export default Index;
