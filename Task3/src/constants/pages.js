29d1582df09f20d555c8a148adc30fcc1b3ef780
eXetrum <shishimorovmaksim@gmail.com>
2024-05-14 20:14:10 +0300

    create constants for author/author-list book/book-list routes

--- Task3/src/constants/pages.js
+++ Task3/src/constants/pages.js
@@ -1,3 +1,7 @@
 export const defaultPage = 'default';
 export const login = 'login';
 export const secretPage = 'secret';
+export const authorPage = 'author';
+export const authorListPage = 'authorList';
+export const bookPage = 'book';
+export const bookListPage = 'bookList';
--- Task3/src/constants/pagesURLs.js
+++ Task3/src/constants/pagesURLs.js
@@ -5,6 +5,10 @@ const result = {
   [pages.defaultPage]: `${config.UI_URL_PREFIX}/${pages.defaultPage}`,
   [pages.login]: `${config.UI_URL_PREFIX}/${pages.login}`,
   [pages.secretPage]: `${config.UI_URL_PREFIX}/${pages.secretPage}`,
+  [pages.authorPage]: `${config.UI_URL_PREFIX}/${pages.authorPage}`,
+  [pages.authorListPage]: `${config.UI_URL_PREFIX}/${pages.authorListPage}`,
+  [pages.bookPage]: `${config.UI_URL_PREFIX}/${pages.bookPage}`,
+  [pages.bookListPage]: `${config.UI_URL_PREFIX}/${pages.bookListPage}`,
 };
 
 export default result;
